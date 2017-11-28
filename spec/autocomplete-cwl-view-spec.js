6827d4aa10e4b5c5ec2a2ddac8a19783d705f0b2
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-11-28 11:02:45 +0900

    Support autocomplete CWL

---
+++ spec/autocomplete-cwl-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutocompleteCwlView from '../lib/autocomplete-cwl-view';
+
+describe('AutocompleteCwlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
